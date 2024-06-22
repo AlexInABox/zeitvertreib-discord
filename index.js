b3784bf2a19b1c9b20c4e1e3d7bc0a6af1b9617c
AlexInABox <main@alexinabox.de>
2024-06-22 13:34:51 +0200

    add new commands to start and stop the server

--- index.js
+++ index.js
@@ -1,79 +1,197 @@
 import { REST, Routes, Client, GatewayIntentBits, ApplicationCommandManager } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config()
+import util from 'util';
+import request from 'request';
+const requestPromise = util.promisify(request);
 
 const commands = [
-    {
-        name: 'ping',
-        description: 'Replies with Pong!',
-    },
-    {
-        name: 'reinstall',
-        description: 'Reinstalls the SCP:SL server.',
-    },
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+  {
+    name: 'reinstall',
+    description: 'Reinstall the SCP:SL server.',
+  },
+  {
+    name: 'start',
+    description: 'Start the SCP:SL server.',
+  },
+  {
+    name: 'stop',
+    description: 'Stop the SCP:SL server.',
+  },
 ];
 
-const TOKEN = 'OTgwNTQyMjcwMzE3MTQ2MTEz.GfrfxF.Nj7oLAtY1SoPri3qHIngJRLiTUHFSsH8DkrVKc';
-const CLIENT_ID = '980542270317146113';
+const TOKEN = process.env.DISCORD_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const AUTHORIZED_USER_ID = process.env.AUTHORIZED_USER_ID;
 
 const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 try {
-    console.log('Started refreshing application (/) commands.');
+  console.log('Started refreshing application (/) commands.');
 
-    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
 
-    console.log('Successfully reloaded application (/) commands.');
+  console.log('Successfully reloaded application (/) commands.');
 } catch (error) {
-    console.error(error);
+  console.error(error);
 }
 
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 
 
-const pTOKEN = "ptla_nLzCYtiYli8BPsSXLd1N9qDVUEkuPMUFuGY2JVNcU6K";
-const pURL = "https://panel.alexinabox.de/api/application/servers/1/reinstall";
-import fetch from 'node-fetch';
+const PANEL_APPLICATION_TOKEN = process.env.PANEL_APPLICATION_TOKEN;
+const PANEL_CLIENT_TOKEN = process.env.PANEL_CLIENT_TOKEN;
+const PANEL_BASE_URL = process.env.PANEL_BASE_URL;
+const SERVER_APPLICATION_ID = process.env.SERVER_APPLICATION_ID;
+const SERVER_CLIENT_ID = process.env.SERVER_CLIENT_ID;
 
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
+  if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+
+  if (interaction.commandName === 'reinstall') {
+    if (!isUserAuthorized(interaction.user.id)) { //if not the owner
+      await interaction.reply('You do not have permission to use this command.');
+      return;
+    }
+    await interaction.reply('Reinstalling...');
+    try{
+      await reinstallServer(PANEL_BASE_URL, SERVER_APPLICATION_ID, PANEL_APPLICATION_TOKEN)
+    } catch (e){
+      await interaction.editReply('Error: ' + e);
     }
 
-    if (interaction.commandName === 'reinstall') {
-        //Check if user id is 428870593358594048
-        if (interaction.user.id !== '428870593358594048') { //if not the owner
-            await interaction.reply('You do not have permission to use this command.');
-            return;
-        }
-        await interaction.reply('Reinstalling...');
-        //Post request to pURL with pTOKEN as Authorization Bearer
-        const headers = {
-            "Authorization": "Bearer " + pTOKEN,
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        }
+    await interaction.editReply(`Reinstalling server. Please wait...`);
 
-        const request = await fetch(pURL, { method: 'POST', headers: headers });
-        var response;
-        try {
-            response = await request.json();
-            if (response.errors) {
-                await interaction.editReply('Error: ' + response.errors[0].detail);
-                return;
-            }
-        } catch (error) {
-            console.log("Success!");
-        }
+    const maxDuration = 2 * 60 * 1000; // 2 minutes in milliseconds
+    const intervalDuration = 5 * 1000; // 5 seconds in milliseconds
+    let elapsedTime = 0;
 
-        await interaction.editReply('Reinstallation initiated. Check status here: https://panel.alexinabox.de/server/3ea2a591');
+    const interval = setInterval(async () => {
+      elapsedTime += intervalDuration;
+
+      if (!await isServerInstalling(PANEL_BASE_URL, SERVER_CLIENT_ID, PANEL_CLIENT_TOKEN)) {
+        clearInterval(interval);
+        await interaction.editReply('Starting server...');
+        try{
+          await sendPowerEventToServer("start", PANEL_BASE_URL, SERVER_CLIENT_ID, PANEL_CLIENT_TOKEN)
+        } catch (e){
+          await interaction.editReply('Error: ' + e);
+        }
+        await interaction.editReply('Server has been successfully reinstalled!');
+        return;
+      }
+
+      if (elapsedTime >= maxDuration) {
+        clearInterval(interval);
+        await interaction.editReply('Reinstallation process did not complete within the expected time frame.');
+        return;
+      }
+    }, intervalDuration);
+  }
+
+  if (interaction.commandName === 'start') {
+    if (!isUserAuthorized(interaction.user.id)) { //if not the owner
+      await interaction.reply('You do not have permission to use this command.');
+      return;
     }
+    await interaction.reply('Starting server...');
+    try{
+      await sendPowerEventToServer("start", PANEL_BASE_URL, SERVER_CLIENT_ID, PANEL_CLIENT_TOKEN)
+    } catch (e){
+      await interaction.editReply('Error: ' + e);
+    }
+
+    await interaction.editReply(`Started server! Check status here: ${PANEL_BASE_URL}server/${SERVER_CLIENT_ID}`);
+  }
+
+  if (interaction.commandName === 'stop') {
+    if (!isUserAuthorized(interaction.user.id)) { //if not the owner
+      await interaction.reply('You do not have permission to use this command.');
+      return;
+    }
+    await interaction.reply('Starting server...');
+    try{
+      await sendPowerEventToServer("stop", PANEL_BASE_URL, SERVER_CLIENT_ID, PANEL_CLIENT_TOKEN)
+    } catch (e){
+      await interaction.editReply('Error: ' + e);
+    }
+
+    await interaction.editReply(`Started server! Check status here: ${PANEL_BASE_URL}server/${SERVER_CLIENT_ID}`);
+  }
 });
+
+async function sendPowerEventToServer(signal, BASE_URL, SERVER_CLIENT_ID, PANEL_CLIENT_TOKEN){
+  const options = {
+    'method': 'POST',
+    'url': `${BASE_URL}api/client/servers/${SERVER_CLIENT_ID}/power`,
+    'headers': {
+      'Authorization': 'Bearer ' + PANEL_CLIENT_TOKEN,
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    formData: {
+      'signal': signal
+    }
+  };
+
+  await request(options, function (error, response) {
+    if (error) throw new Error(error);
+  });
+}
+
+async function reinstallServer(BASE_URL, SERVER_APPLICATION_ID, PANEL_APPLICATION_TOKEN){
+  const options = {
+    'method': 'POST',
+    'url': `${BASE_URL}api/application/servers/${SERVER_APPLICATION_ID}/reinstall`,
+    'headers': {
+      'Authorization': 'Bearer ' + PANEL_APPLICATION_TOKEN,
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  };
+
+  await request(options, function (error, response) {
+    if (error) throw new Error(error);
+  });
+}
+
+async function isServerInstalling(BASE_URL, SERVER_CLIENT_ID, PANEL_CLIENT_TOKEN){
+  const options = {
+    'method': 'GET',
+    'url': `${BASE_URL}api/client/servers/${SERVER_CLIENT_ID}/`,
+    'headers': {
+      'Authorization': 'Bearer ' + PANEL_CLIENT_TOKEN,
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  };
+
+  try {
+    const response = await requestPromise(options);
+    const isInstalling = JSON.parse(response.body).attributes.is_installing;
+    return isInstalling === true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+function isUserAuthorized(userID) {
+  return userID === AUTHORIZED_USER_ID;
+}
 
 client.login(TOKEN);
