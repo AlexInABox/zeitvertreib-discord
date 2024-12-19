0ed82ca07f083885cf4d1056531e09b6e21cdee2
alexinabox <main@alexinabox.de>
2024-12-19 13:58:59 +0100

    prettier

--- setupCommands.js
+++ setupCommands.js
@@ -3,72 +3,74 @@ const DELETE_BEFORE = false;
 
 // Commands to be defined
 const COMMANDS = [
-    {
-      name: 'ping',
-      description: 'Replies with Pong!',
-    },
-    {
-      name: 'reinstall',
-      description: 'Reinstall the SCP:SL server.',
-    },
-    {
-      name: 'restart',
-      description: 'Restart the SCP:SL server.',
-    },
-    {
-      name: 'start',
-      description: 'Start the SCP:SL server.',
-    },
-    {
-      name: 'stop',
-      description: 'Stop the SCP:SL server.',
-    },
-    {
-      name: 'playerlist',
-      description: 'Get a list of players on the SCP:SL server.',
-    }
-  ];
-  
+  {
+    name: "ping",
+    description: "Replies with Pong!",
+  },
+  {
+    name: "reinstall",
+    description: "Reinstall the SCP:SL server.",
+  },
+  {
+    name: "restart",
+    description: "Restart the SCP:SL server.",
+  },
+  {
+    name: "start",
+    description: "Start the SCP:SL server.",
+  },
+  {
+    name: "stop",
+    description: "Stop the SCP:SL server.",
+  },
+  {
+    name: "playerlist",
+    description: "Get a list of players on the SCP:SL server.",
+  },
+];
 
-import { REST, Routes } from 'discord.js';
-import dotenv from 'dotenv';
+import { REST, Routes } from "discord.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-if(!process.env.DISCORD_TOKEN) {
+if (!process.env.DISCORD_TOKEN) {
   console.warn("Could not register discord commands since no token was found!");
-  process.exit(); 
+  process.exit();
 }
 
 const TOKEN = process.env.DISCORD_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
 
-const rest = new REST({ version: '10' }).setToken(TOKEN);
+const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 try {
-    if (DELETE_BEFORE) await deleteAllCommands();
-    await registerAllCommands();
-
+  if (DELETE_BEFORE) await deleteAllCommands();
+  await registerAllCommands();
 } catch (error) {
-    console.error(error);
+  console.error(error);
 }
 
 async function deleteAllCommands() {
-    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: [] })
-        .then(() => console.log('Successfully deleted all guild commands.'))
-        .catch(console.error);
+  await rest
+    .put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: [] })
+    .then(() => console.log("Successfully deleted all guild commands."))
+    .catch(console.error);
 
-    // for global commands
-    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: [] })
-        .then(() => console.log('Successfully deleted all application commands.'))
-        .catch(console.error);
+  // for global commands
+  await rest
+    .put(Routes.applicationCommands(CLIENT_ID), { body: [] })
+    .then(() => console.log("Successfully deleted all application commands."))
+    .catch(console.error);
 }
 
 async function registerAllCommands() {
-    console.log('Started refreshing application (/) commands.');
+  console.log("Started refreshing application (/) commands.");
 
-    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: COMMANDS });
+  await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+    body: COMMANDS,
+  });
 
-    console.log('Successfully reloaded application (/) commands.');
+  console.log("Successfully reloaded application (/) commands.");
 }
